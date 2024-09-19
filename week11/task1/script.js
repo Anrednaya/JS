573202dd8f4f5d71a71cc661662dbaeb0870cd7b
Anrednaya <Antonina.rednaya@gmail.com>
2024-09-19 22:48:23 +0500

    made todolist

---
+++ week11/task1/script.js
@@ -0,0 +1,21 @@
+function addTask() {
+  const task = document.getElementById('task');
+  const newItem = document.createElement('li');
+  if (task.value == '') return 
+  else {
+    newItem.textContent = task.value;
+    const tasklist = document.getElementById('tasklist');
+    tasklist.append(newItem);
+    const del = document.createElement('span');
+    del.textContent = 'удалить';
+    newItem.append(del);
+    del.addEventListener ('click', function () {
+      this.closest('li').remove();
+    });
+    newItem.addEventListener ('click', function () {
+      this.classList.toggle('done');
+    });
+  }
+};
+
+document.getElementById('addButton').onclick = addTask;
